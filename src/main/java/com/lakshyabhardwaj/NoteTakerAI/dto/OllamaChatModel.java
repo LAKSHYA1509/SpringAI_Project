83eb9244ae7a2c89aa98a69b695095c6fedd08ee
LAKSHYA BHARDWAJ <lakshyabhardwaj200315@gmail.com>
2025-01-26 15:17:09 +0530

    Made code cleaner

--- src/main/java/com/lakshyabhardwaj/NoteTakerAI/dto/OllamaChatModel.java
+++ src/main/java/com/lakshyabhardwaj/NoteTakerAI/dto/OllamaChatModel.java
@@ -3,7 +3,6 @@ package com.lakshyabhardwaj.NoteTakerAI.dto;
 public class OllamaChatModel {
 
     public String call(String promptText) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'call'");
     }
     
